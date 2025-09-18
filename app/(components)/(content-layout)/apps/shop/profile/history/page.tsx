cbf55931dca1917fe3eddcd8d0c3e16658e91a7b
Kubilay-web <kubilayozdemir95@gmail.com>
2025-09-18 16:47:06 +0300

    Updated

--- app/(components)/(content-layout)/apps/shop/profile/history/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function ProfileHistoryPage() {
-  redirect("/apps/shop/profile/history/1");
-}
--- app/(components)/(content-layout)/apps/shop/profile/wishlist/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function ProfileWishlistPage() {
-  redirect("/apps/shop/profile/wishlist/1");
-}
