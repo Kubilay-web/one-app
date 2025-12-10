1e59e97dfadf1afbcc573bc480e260c6cb1285ba
Kubilay-web <kubilayozdemir95@gmail.com>
2025-12-10 15:00:11 +0300

    Updated

--- app/(components)/(content-layout)/home/onesocial/(social)/settings/account/components/AccountSettings.tsx
+++ app/(components)/(content-layout)/home/onesocial/(social)/settings/account/components/AccountSettings.tsx
@@ -69,7 +69,7 @@ const ChangePassword = () => {
               type="submit" 
               className="rounded-lg bg-blue-600 px-6 py-2.5 text-sm font-medium text-white transition-colors hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
             >
-              Update password
+              Update Password
             </button>
           </div>
         </form>
