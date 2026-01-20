cd48490350a6c7a8df3564629126f06cd19e4202
Kubilay-web <kubilayozdemir95@gmail.com>
2026-01-20 14:26:23 +0300

    Updated

--- app/(components)/(content-layout)/realestate/(private)/user/properties/page.tsx
+++ app/(components)/(content-layout)/realestate/(private)/user/properties/page.tsx
@@ -35,6 +35,12 @@ async function Properties({
     <div>
       <div className="flex justify-between items-center">
         <PageTitle title="Properties" />
+        <Link
+          href="/realestate/user/properties/create-property"
+          className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors duration-200"
+        >
+          Create Property
+        </Link>
       </div>
       <Filters searchParams={params} />
       <Suspense fallback={<Loader />} key={key}>
